a59738cd504c5d5c7ac746c15ee5c2a30b71e6ae
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-01-16 14:11:49 +0200

    fix?

--- console.py
+++ console.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ Console Module """
 import cmd
-from shlex import shlex
+import shlex
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
