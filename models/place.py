4ac743c1eecf3e9bddd183c3a8d65cb711d2856a
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-01-17 23:51:56 +0200

    fix

--- models/place.py
+++ models/place.py
@@ -20,7 +20,7 @@ place_amenity = Table(
         ForeignKey("amenities.id"),
         nullable=False))
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
