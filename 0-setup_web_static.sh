38675e374e3bd9631b48eaee90e3f94b8d139fc7
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-01-31 16:48:01 +0200

    prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# prepare your web servers
+
+# install nginx if it's not installed
+sudo apt-get -y update
+sudo apt-get -y install nginx
+
+# create folders if they don't already exist
+if [ ! -d /data/web_static/releases/test/ ] then
+  sudo mkdir -p /data/web_static/releases/test/
+fi
+
+# create fake html file
+echo "Hello World!" > /data/web_static/releases/test/index.html
+
+# create symbolic link
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+# change ownership of data folder
+sudo chown -R ubuntu:ubuntu /data
+
+# update nginx config
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+	alias /data/web_static/current;
+	index index.html index.htm;
+    }
+
+    location /redirect_me {
+	return 301 http://youtube.com/;
+    }
+
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+sudo service nginx restart
