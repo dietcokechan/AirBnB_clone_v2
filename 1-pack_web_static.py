e453009e8291af6aa884ee486bc080340b652a8d
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-01 17:03:00 +0200

    compress before sending

---
+++ 1-pack_web_static.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""compress before sending"""
+import os.path
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """generate .tgz archive from folder content"""
+    date = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date.year,
+                                                         date.month,
+                                                         date.day,
+                                                         date.hour,
+                                                         date.minute,
+                                                         date.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
+        return None
+    return file
