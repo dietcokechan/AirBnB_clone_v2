e11d45321cb99af4d218cd5514ec75905614b577
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-01 17:02:46 +0200

    fix?

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,8 +6,11 @@ sudo apt-get -y update
 sudo apt-get -y install nginx
 
 # create folders if they don't already exist
-sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
 
 # create fake html file
 echo "Hello World!" > /data/web_static/releases/test/index.html
@@ -19,27 +22,8 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 
 # update nginx config
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-    alias /data/web_static/current;
-    index index.html index.htm;
-    }
-
-    location /redirect_me {
-    return 301 http://google.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+file=/etc/nginx/sites-available/default
+str="location /hbnb_static/{\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "27i $str" $file
 
 sudo service nginx restart
