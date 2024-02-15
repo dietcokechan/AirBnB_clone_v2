28cdb231392ea11deeb5ecc1100f437bd65189c1
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-15 22:50:59 +0200

    python is cool

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""hello flask"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_is(text):
+    res = text.replace("_", " ")
+    return f"C {res}"
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_is(text="is cool"):
+    res = text.replace("_", " ")
+    return f"Python {res}"
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
