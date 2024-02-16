1108ddff1dbcf66326206b1fdaf08790195b9e49
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-16 21:53:48 +0200

    states and state

---
+++ web_flask/9-states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""hello flask"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/states")
+def get_states_list():
+    return render_template("8-cities_by_states.html", storage=storage.all('State'))
+
+
+@app.route("/states/<id>")
+def get_cities(id):
+    obj = storage.all('State').get('State.{}'.format(id))
+    return render_template("8-cities_by_states.html", storage=storage.all('State'), obj=obj)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
