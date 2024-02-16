93ef85fd558313cd8b79f30fa72e8952d6022af0
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-16 17:02:05 +0200

    moved option to if statement

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,9 +5,10 @@ from flask import Flask;
 
 app = Flask(__name__)
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def index():
     return "Hello HBNB!"
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,19 +6,20 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/")
 def index():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     return "HBNB"
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c_is(text):
     res = text.replace("_", " ")
     return f"C {res}"
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
