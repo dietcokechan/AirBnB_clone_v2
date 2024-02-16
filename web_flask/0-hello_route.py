23f1b52838d83e2ecb2bf24ad0bb54fea2eac3b9
Habiba Sameh <68277372+dietcokechan@users.noreply.github.com>
2024-02-16 17:09:36 +0200

    fix?

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,12 +6,11 @@ from flask import Flask;
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     """index"""
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
